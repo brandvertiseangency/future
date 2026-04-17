c5ee4e4cdc53db602387166593e3c227153d4f2d
brandvertiseagency <zotaxfilling@gmail.com>
2026-04-17 22:40:53 +0530

    chore: remove macOS junk files and dev build cache from repo

--- frontend/Users/ryhnsufi/.brandvertise-next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"R5taZvzhjelYPkbrM7WsHEnrc9wyL+CWXGPXrMfH8T0=\"\n}"
--- frontend/Users/ryhnsufi/.brandvertise-next/dev/static/chunks/src_app_page_tsx_0q__6b4._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_00klu~4._.js","static/chunks/node_modules_0gnmc4u._.js"],
-    source: "dynamic"
-});
